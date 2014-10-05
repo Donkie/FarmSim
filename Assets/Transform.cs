b3bcbfb50f6c234d363415a7113f49394cfc3e3d
Daniel <daniel.cf.hultgren@gmail.com>
2014-10-05 19:42:21 +0200

    Component approach x2

---
+++ Assets/Transform.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class Transform : MonoBehaviour
+    {
+        public string Name;
+        public int Id;
+        public string IndexPath;
+        public bool Visibility;
+        public int ClipDistance;
+        public int MinClipDistance;
+        public int ObjectMask;
+        public bool LOD;
+        public bool RigidBody;
+        public bool Joint;
+    }
+}
